6ffdb577ad285d6f6f372b1c5284f07ca2c9ad88
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2023-10-04 21:10:33 -0400

    Aldo/katas added 3 katas

---
+++ src/test/java/com/qaacademy/module3/tddgit/aldo/katas/CCTest.java
@@ -0,0 +1,16 @@
+package com.qaacademy.module3.tddgit.aldo.katas;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CCTest {
+    @Test
+    public void testSomething() {
+        assertEquals(4, CC.charCount("fizzbuzz", 'z'));
+        assertEquals(4, CC.charCount("FIZZBUZZ", 'z'));
+    }
+}
